89a161415c7cd8aff7cae7a4093968d01af12cfe
Lawrence <lawrence.narh-lawson@boomin.com>
2021-10-07 00:31:44 +0100

    Fetched movies and displayed via API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
   function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films');
+    fetch('https://swapi.dev/api/films')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
   }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
